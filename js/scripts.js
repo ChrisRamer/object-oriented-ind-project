61dcbf6f263329ae519915315961a95c174c16ad
Chris Ramer <chrisramer.97@gmail.com>
2020-10-05 11:43:52 -0700

    Add delivery form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 // Business Logic
 
-
+function ShowDeliveryForm() {
+	$(".deals-row").hide();
+	$("#deliveryForm").fadeIn(500);
+}
 
 // User interface logic
 
 $(document).ready(function () {
+	$("#deliver").click(function (e) { 
+		e.preventDefault();
+		ShowDeliveryForm();
+	});
 
+	$("#delivery-form").submit(function (e) { 
+		e.preventDefault();
+		console.log("Form submitted!");
+	});
 });
