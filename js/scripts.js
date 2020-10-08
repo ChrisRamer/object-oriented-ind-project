83a8fc25cba58d704ba847e92668bd7a1e49bba4
Chris Ramer <chrisramer.97@gmail.com>
2020-10-08 09:13:27 -0700

    Make pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,44 @@
 // Business Logic
 
+const tax = 1234567890;
+
+function Pizza() {
+	this.size = "Large";
+	this.toppings = [];
+	this.crust = "Regular";
+}
+
+Pizza.prototype.makePizza = function(size, toppings, crust) {
+	this.size = size;
+	this.toppings = toppings;
+	this.crust = crust;
+}
+
+Pizza.prototype.getPrice = function() {
+	let price;
+
+	switch (this.size) {
+		case Small:
+			price += 500;
+			break;
+		case Medium:
+			price += 1000;
+			break;
+		case Large:
+			price += 2000;
+			break;
+		case ExtraLarge:
+			price += 99999999;
+			break;
+	}
+
+	for (var i = 0; i < this.toppings.length; i++) {
+		price += 1000;
+	}
+
+	return price;
+}
+
 function ShowDeliveryForm() {
 	$(".deals-row").hide();
 	$("#deliveryForm").fadeIn(500);
@@ -7,14 +46,19 @@ function ShowDeliveryForm() {
 
 // User interface logic
 
+let pizza = new Pizza();
+
 $(document).ready(function () {
 	$("#deliver").click(function (e) { 
 		e.preventDefault();
 		ShowDeliveryForm();
 	});
 
-	$("#delivery-form").submit(function (e) { 
+	$("#dilivery-form").submit(function (e) { 
 		e.preventDefault();
-		console.log("Form submitted!");
+		const size = $("#pizzaSizeInput").val();
+		const toppings = $("#pizzaToppingsInput").val();
+		const crust = $("#pizzaCrustInput").val();
+		pizza.makePizza(size, toppings, crust);
 	});
 });
